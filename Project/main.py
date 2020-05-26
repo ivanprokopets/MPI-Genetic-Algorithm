1b92f9fead26d79f83a6020877f4590385bf098d
ivanprokopets <ivanprokopets1999@gmail.com>
2020-05-26 22:32:46 +0200

    Zmiany w GUI, dodano nowa klasa. Zmieniona metoda OptimizerGA

--- Project/main.py
+++ Project/main.py
@@ -1,26 +1,15 @@
-from function import Func
-from optimizer import OptimizerGA
-from GUI import get_settings, info_GUI, error_GUI
-from settingsGA import gaParams
-
+from GUI import gaGUI
 
 # (x ** 1/2 - 5 * y ) / (x ** 2 + y ** 2  - 2 * x + 10) - przykladowa Funkcja do testowania
 
-def main():
-    try:
-        while True:
-            gaParams = get_settings()
-            function = Func(gaParams.f)
-            optimizer = OptimizerGA(function)
-            optimizer.startGA_with_statistics(chromosomes_number=gaParams.chromosomes_number,
-                                              generations_number=gaParams.generations_number,
-                                              mutation=gaParams.mutation, optimizer=gaParams.optimizer)
-            info_GUI()
+from GUI import gaGUI
 
-    except Exception as err:
-        print('BLAD!\n', type(err))
-        print(err)
-        error_GUI()
+global random_seed
+random_seed = 1
+
+
+def main():
+    gui = gaGUI()
 
 
 if __name__ == '__main__':
